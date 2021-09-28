41499fb14e1baf10e91adbed3061d04f49fcbd83
James Eastoe-Collins <jamespecollins@gmail.com>
2021-09-28 17:55:18 +1000

    Add Routing ad site styles

--- jobs-frontend/src/App.js
+++ jobs-frontend/src/App.js
@@ -1,24 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
+import SearchForm from './Components/SearchForm/SearchForm.js';
+import JobResults from './Components/JobResults/JobResults.js';
+import Hero from './Components/Hero/Hero.js';
+import Header from './Components/Header/Header.js';
+import Footer from './Components/Footer/Footer';
+
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 
 function App() {
+
+  const NotFound = () => {
+    return (<div className="Container p-5"><h2>Something went wrong and we couldnt find what you want try making a search!</h2></div>)
+  }
+
+  // TODO re-arrange switch block
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="App d-flex flex-column min-vh-100">
+
+      <Router>
+
+        <Header />
+        <Route exact path="/" >
+          <Hero />
+
+        </Route>
+
+        <SearchForm />
+
+        <Switch>
+          <Route exact path="/" >
+          </Route>
+
+          <Route exact path="/search">
+            <JobResults />
+          </Route >
+
+          <Route component={NotFound} >
+            <NotFound />
+          </Route>
+
+
+        </Switch>
+
+        <Footer />
+      </Router>
+
+
+    </div >
   );
 }
 
---
+++ jobs-frontend/src/Components/Footer/Footer.js
@@ -0,0 +1,11 @@
+
+const Footer = (props) => {
+
+    return (
+        <footer className="footer mt-auto p-5">
+            This is a footer
+        </footer>
+    )
+}
+
+export default Footer;
